519ad48a7044b5eaa4a5c403ffe2620c1e3a3daf
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2023-11-13 08:15:50 -0300

    eitaaa

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-javaaaaaa
+const tarefa = document.querySelector('#tarefa')
+const btn = document.querySelector("#btn")
+const lista = document.querySelector("#lista")
+
+btn.addEventListener("click", function(){
+    if (tarefa.value == ""){
+        alert("Digite uma tarefa válida né!")
+    }  else {
+        lista.innerHTML += `  <li> 
+        <i class="fas fa-check-circle check"></i>
+        <span>${tarefa.value}</span>
+        <i class="fa-solid fa-trash-can close"></i>
+        </li>`
+    }
+
+})
